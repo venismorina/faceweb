9daddf2ff55b6ceb17ac8ef52794fccf747315e5
venismorina <venismorinax8@gmail.com>
2021-05-20 17:15:44 +0200

    change

--- facetimer/wsgi.py
+++ facetimer/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for bambusface project.
+WSGI config for facetimer project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bambusface.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facetimer.settings')
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bambusface.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facetimer.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
